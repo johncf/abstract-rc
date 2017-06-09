6b22ed9a8d268cefb391cef724c286ee8978596f
johncf <johncf@protonmail.com>
2017-06-09 10:26:03 +0530

    +make_mut

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
-/// The abstract interface for a reference counted type.
-pub trait RefCounted: Clone {
-    type Target;
+use std::ops::Deref;
 
+/// The abstract interface for a reference counted type.
+pub trait MutablePtr: Deref {
     fn new(obj: Self::Target) -> Self;
-    fn strong_count(this: &Self) -> usize;
+    fn make_mut(this: &mut Self) -> &mut Self::Target;
 }
 
-//The following impl is useless! See the test `it_works` for a detailed explanation and workaround.
+// The following impl isn't useful! See `it_works` for a detailed explanation and workaround.
 //
-//use std::rc::Rc;
-//impl<T> RefCounted for Rc<T> {
-//    type Target = T;
-//
-//    fn new(obj: T) -> Rc<T> {
-//        Rc::new(obj)
-//    }
-//
-//    fn strong_count(this: &Rc<T>) -> usize {
-//        Rc::strong_count(this)
-//    }
-//}
+//     impl<T: Clone> MutablePtr for std::rc::Rc<T> { ... }
 
 #[cfg(test)]
 mod tests {
-    use super::RefCounted;
+    use super::MutablePtr;
 
+    use std::ops::Deref;
     use std::rc::Rc;
     use std::sync::Arc;
 
     struct Hello<T, RC>
-    where RC: RefCounted<Target=Hello<T, RC>>
+    where RC: MutablePtr<Target=Hello<T, RC>>
     {
         val: T,
         next: Option<RC>,
     }
 
-    impl<T, RC> Hello<T, RC> where RC: RefCounted<Target=Hello<T, RC>> {
+    impl<T, RC> Clone for Hello<T, RC>
+    where T: Clone,
+          RC: MutablePtr<Target=Hello<T, RC>> + Clone,
+    {
+        fn clone(&self) -> Self {
+            Hello {
+                val: self.val.clone(),
+                next: self.next.clone(),
+            }
+        }
+    }
+
+    impl<T, RC> Hello<T, RC> where RC: MutablePtr<Target=Hello<T, RC>> {
         fn new(val: T) -> Hello<T, RC> {
             Hello { val: val, next: None }
         }
@@ -54,7 +56,7 @@ mod tests {
         //
         // Therefore we need to create newtypes which specifies both the wrapper type as well as
         // the target type. This means, for a given target type, we need to define newtypes which
-        // implement RefCounted for every wrapper type we want to abstract over.
+        // implement MutablePtr for every wrapper type we want to abstract over.
         //
         // This can be made easier by defining macros that generate such impls.
 
@@ -65,45 +67,89 @@ mod tests {
         // using methods would need annotations
         let mut hello_rc = Hello::<_, HelloRc<_>>::new(0);
         hello_rc.set_next(1);
+
+        struct NotClone;
+
+        // using methods would need annotations
+        let mut hello_box = Hello::<_, HelloBox<_>>::new(NotClone);
+        hello_box.set_next(NotClone);
     }
 
-    struct HelloRc<T>(Rc<Hello<T, HelloRc<T>>>);
+    struct HelloRc<T: Clone>(Rc<Hello<T, HelloRc<T>>>);
 
-    struct HelloArc<T>(Arc<Hello<T, HelloArc<T>>>);
+    struct HelloArc<T: Clone>(Arc<Hello<T, HelloArc<T>>>);
 
-    impl<T> RefCounted for HelloRc<T> {
+    struct HelloBox<T>(Box<Hello<T, HelloBox<T>>>);
+
+    impl<T: Clone> Deref for HelloRc<T> {
         type Target = Hello<T, Self>;
 
+        fn deref(&self) -> &Self::Target {
+            &*self.0
+        }
+    }
+
+    impl<T: Clone> MutablePtr for HelloRc<T> {
         fn new(obj: Hello<T, Self>) -> Self {
             HelloRc(Rc::new(obj))
         }
 
-        fn strong_count(this: &Self) -> usize {
-            Rc::strong_count(&this.0)
+        fn make_mut(this: &mut Self) -> &mut Self::Target {
+            Rc::make_mut(&mut this.0)
         }
     }
 
-    impl<T> Clone for HelloRc<T> {
+    impl<T: Clone> Clone for HelloRc<T> {
         fn clone(&self) -> Self {
             HelloRc(self.0.clone())
         }
     }
 
-    impl<T> RefCounted for HelloArc<T> {
+    impl<T: Clone> Deref for HelloArc<T> {
         type Target = Hello<T, Self>;
 
+        fn deref(&self) -> &Self::Target {
+            &*self.0
+        }
+    }
+
+    impl<T: Clone> MutablePtr for HelloArc<T> {
         fn new(obj: Hello<T, Self>) -> Self {
             HelloArc(Arc::new(obj))
         }
 
-        fn strong_count(this: &Self) -> usize {
-            Arc::strong_count(&this.0)
+        fn make_mut(this: &mut Self) -> &mut Self::Target {
+            Arc::make_mut(&mut this.0)
         }
     }
 
-    impl<T> Clone for HelloArc<T> {
+    impl<T: Clone> Clone for HelloArc<T> {
         fn clone(&self) -> Self {
             HelloArc(self.0.clone())
+        }
+    }
+
+    impl<T> Deref for HelloBox<T> {
+        type Target = Hello<T, Self>;
+
+        fn deref(&self) -> &Self::Target {
+            &*self.0
+        }
+    }
+
+    impl<T> MutablePtr for HelloBox<T> {
+        fn new(obj: Hello<T, Self>) -> Self {
+            HelloBox(Box::new(obj))
+        }
+
+        fn make_mut(this: &mut Self) -> &mut Self::Target {
+            &mut *this.0
+        }
+    }
+
+    impl<T: Clone> Clone for HelloBox<T> {
+        fn clone(&self) -> Self {
+            HelloBox(self.0.clone())
         }
     }
 }
